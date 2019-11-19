ade648da22eb011a1f70675b9e8c2704f2bc9d80
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-11-19 15:12:23 -0800

    add rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_234355) do
+ActiveRecord::Schema.define(version: 2019_11_19_230633) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "coach"
+    t.string "name"
+    t.string "city"
+    t.string "sport"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
