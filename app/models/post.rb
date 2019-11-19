9d7d973d14527365fc32ea729433b97025b3be7e
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-11-19 15:02:42 -0800

    end of last study group

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   validates :title, presence: true
+  def to_param
+    slug = title.split(/[\s.,\-\!\?]+/).join('-').downcase
+    "#{slug}-#{id}"
+  end
+
+  def self.find_by_slug(slug)
+    id = slug.split('-').last
+    self.find_by_id(id)
+  end
 end
