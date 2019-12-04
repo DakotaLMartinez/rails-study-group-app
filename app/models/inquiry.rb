fe4216b803e727bc53a8be2e42d4004ef72514a1
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-12-03 16:44:44 -0800

    add allow_blank option to inquiry phone_number

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,5 +1,5 @@
 class Inquiry < ApplicationRecord
   validates :name, :email_address, :content, presence: true
-  validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "incorrectly formatted. Please type phone number in 555-555-5555 format" }
+  validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "incorrectly formatted. Please type phone number in 555-555-5555 format" }, allow_blank: true
   validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP, message: "incorrectly formatted. Please check your email and try again" } 
 end
