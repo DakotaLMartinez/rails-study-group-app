27758993b37b722e8a153db0c3d5328ff571a09a
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-12-03 16:20:45 -0800

    add inquiries views

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,2 +1,34 @@
 class InquiriesController < ApplicationController
+
+  def index 
+    @inquiries = Inquiry.all
+  end
+
+  def show 
+    @inquiry = Inquiry.find(params[:id])
+  end
+
+  def new 
+    @inquiry = Inquiry.new
+  end
+
+  def create 
+    inquiry = Inquiry.new(inquiry_params)
+    binding.pry
+  end
+
+  def edit
+
+  end 
+
+  def update 
+    inquiry = Inquiry.find_by_id(params[:id])
+    inquiry.update(inquiry_params)
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email_address, :phone_number, :content)
+  end
 end
